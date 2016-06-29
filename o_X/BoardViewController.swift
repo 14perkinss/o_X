b42d201abcc8edaac39506d5e736200c8a24f52c
Sam Perkins <davidsp@princeton.edu>
2016-06-29 20:35:32 +0200

    Missing button functionality

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -6,7 +6,6 @@
 import UIKit
 
 class BoardViewController: UIViewController {
-
     @IBOutlet weak var boardView: UIView!
     @IBOutlet weak var newGameButton: UIBarButtonItem!
     // Create additional IBOutlets here.
@@ -22,6 +21,7 @@ class BoardViewController: UIViewController {
 
     @IBAction func newGamePressed(sender: UIBarButtonItem) {
         print("New Game Button Pressed")
+        OXGameController.sharedInstance.restartGame()
     }
     
     // Create additional IBActions here.
@@ -30,7 +30,19 @@ class BoardViewController: UIViewController {
     }
 
     @IBAction func cellPressed(sender: AnyObject) {
+        OXGameController.sharedInstance.playMove(sender.tag)
+        var currGame = OXGameController.sharedInstance.getCurrentGame()
+        //sender.title = currGame.whoseTurn()
         print("Button Pressed:", sender.tag)
+        var gameState = currGame.state()
+        if (gameState == OXGameState.Won) {
+            print("Congratulations!")
+            OXGameController.sharedInstance.restartGame()
+        } else if (gameState == OXGameState.Tie) {
+            print("Game ended in a tie")
+            OXGameController.sharedInstance.restartGame()
+        }
+        
     }
     
     var gameObject = OXGame()
---
+++ o_X/OXGameController.swift
@@ -0,0 +1,27 @@
+//
+//  OXGameController.swift
+//  o_X
+//
+//  Created by Sam Perkins on 6/29/16.
+//  Copyright © 2016 iX. All rights reserved.
+//
+
+import UIKit
+
+
+class OXGameController {
+    static let sharedInstance = OXGameController()
+    private var currentGame = OXGame()
+    private init() {}
+    func getCurrentGame() -> OXGame {
+        return currentGame
+    }
+    
+    func restartGame() {
+        currentGame.reset()
+    }
+    
+    func playMove(cell: Int) {
+        currentGame.playMove(cell)
+    }
+}
